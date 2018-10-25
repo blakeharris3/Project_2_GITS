269fe4520234a76c324d1a7280ebb39def64412e
Blake Harris <Blake@Blakes-MBP.local>
2018-10-25 17:47:35 -0500

    copied heroku

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const aboutUsController = require('./controllers/aboutus')
 
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 require('./db/db');
 
     ////////////////////////////////////////////////////////////////////////////////
